9350d638806764c37e94ea882f1f6ff170ea613a
Jastot <george1005@yandex.ru>
2021-02-28 22:14:18 +0300

    2 Lesson

---
+++ 2D_Platformer/Assets/Scripts/Enemies/Gun/BulletsForCannon/BulletView.cs
@@ -0,0 +1,18 @@
+﻿using Managers;
+using Player;
+using UnityEngine;
+
+namespace Enemies.Gun.BulletsForCannon
+{
+    public class BulletView: ObjectOnSceneView
+    {
+        public TrailRenderer _trail;
+
+        public void SetVisible(bool visible)
+        {
+            if (_trail) _trail.enabled = visible;
+            if (_trail) _trail.Clear();
+            SpriteRenderer.enabled = visible;
+        }
+    }
+}
--- 2D_Platformer/Assets/Scripts/Player/PlayerView.cs
+++ 2D_Platformer/Assets/Scripts/Player/PlayerView.cs
@@ -1,13 +1,10 @@
-﻿using UnityEngine;
+﻿using Managers;
+using UnityEngine;
 
 namespace Player
 {
-    public class PlayerView
+    public class PlayerView: ObjectOnSceneView
     {
-        public GameObject Player;
-        public Transform Transform;
-        public SpriteRenderer SpriteRenderer;
-        public Rigidbody2D Rigidbody2D;
-        public Collider2D Collider2D;
+        
     }
 }
